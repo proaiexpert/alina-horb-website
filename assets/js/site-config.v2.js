7986cad9bdd3549be56444e5021dc9bf74ecc21e
Ihor Horb <proai.expert2026@gmail.com>
2026-07-14 23:01:02 -0700

    Load privacy and safe-intake styles on homepage

--- assets/js/site-config.v2.js
+++ assets/js/site-config.v2.js
@@ -20,6 +20,7 @@ window.ALINA_SITE_CONFIG = {
 
   appendStylesheet("../css/site.v3-1-stability.css");
   appendStylesheet("../css/site.footer.v3-2.css");
+  appendStylesheet("../css/site.privacy.v3-2.css");
 
   const faviconUrl = new URL("../images/logos/favicon-ag.svg", script.src).href;
   let favicon = document.querySelector('link[rel~="icon"]');
