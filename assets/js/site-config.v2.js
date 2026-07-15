637aaa31f3945ab340a458f244ebbd8c5f791723
Ihor Horb <proai.expert2026@gmail.com>
2026-07-15 16:24:27 -0700

    Stage production Formspree endpoint for launch

--- assets/js/site-config.v2.js
+++ assets/js/site-config.v2.js
@@ -1,6 +1,7 @@
 window.ALINA_SITE_CONFIG = {
   email: "hello@alinahorb.com",
   telegramUsername: "alina_horb1991",
-  formEndpoint: "",
-  formMode: "mailto"
+  formEndpoint: "https://formspree.io/f/mvzezana",
+  formMode: "formspree",
+  turnstileSiteKey: ""
 };
